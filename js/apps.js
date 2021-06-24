2c4315ff49bc982492b42434389b0c699e7ffb82
pchincoa <pchincoa@gmail.com>
2021-06-24 19:52:13 +0200

    Esverde Template & Commit 0.1

---
+++ js/apps.js
@@ -0,0 +1,18 @@
+//ScrollReveal().reveal('.content-header__title', { delay: 500 });
+//ScrollReveal().reveal('.content-header__img', { delay: 500 });
+ScrollReveal().reveal('.team-item', { delay: 500 });
+ScrollReveal().reveal('.gartnerei-item', { delay: 500 });
+ScrollReveal().reveal('.landschaft-text', { delay: 500 });
+ScrollReveal().reveal('.floristik-text', { delay: 500 });
+ScrollReveal().reveal('.floristik-services', { delay: 500 });
+ScrollReveal().reveal('.gallery-item', { delay: 500 });
+ScrollReveal().reveal('.container-form', { delay: 500 });
+ScrollReveal().reveal('.garten-text', { delay: 500 });
+
+
+// GESAP ANIMATIONS
+
+
+
+
+
